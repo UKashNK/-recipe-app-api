ae022aa6d5e43e38f70e95a326ce7485733f16b1
Katende Ndugwa <161112989+UKashNK@users.noreply.github.com>
2024-05-26 12:51:58 +0300

    configuring DB_HOST

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -17,7 +17,7 @@ class CommandTests(SimpleTestCase):
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database is ready"""
         patched_check.return_value = True
-
+        
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=["default"])
@@ -32,4 +32,3 @@ class CommandTests(SimpleTestCase):
 
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
-
